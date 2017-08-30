798f4b6e6f503d4b0d3a46464e4dc796bbc87f7b
Ivan F <31392888+twistedfascination@users.noreply.github.com>
2017-08-29 19:54:28 -0700

    Do while for rand()
    Commented out

--- main.c
+++ main.c
@@ -8,8 +8,25 @@ int main()
   const int LOW = 0;		// Easy range edit for low
   int flag = 0;
 
+
+  printf("Guess a number between %d and %d", HIGH, LOW);
+	//Prompts user for input
+
+	srand(time(NULL));
+	//Number generator
+
+
+//----------------------------DO WHILE RAND------------------------------------
+  do{
+    int answer = rand();
+  }while(answer < LOW || answer >= HIGH);
+  //Creates a proper distribution of random numbers
+//----------------------------DO WHILE RAND------------------------------------
+
+
+
 	do {
 		printf("Test");
 	} while (flag == 1)
-  
+
 }
