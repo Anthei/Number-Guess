cddba633c317c36460fdf01adcd7004d02a085dd
Anthei <prowler86@gmail.com>
2017-08-29 20:46:41 -0600

    add do-while loop

--- main.c
+++ main.c
@@ -4,9 +4,12 @@
 
 int main()
 {
-  const int HIGH = 11; 	// Easy range edit for high
+  const int HIGH = 11; 		// Easy range edit for high
   const int LOW = 0;		// Easy range edit for low
+  int flag = 0;
 
-
+	do {
+		printf("Test");
+	} while (flag == 1)
   
 }
