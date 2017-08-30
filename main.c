7f7fecbacb03fb5f8fe232e32a8719f0bff5c0a8
Anthei <prowler86@gmail.com>
2017-08-29 21:37:57 -0600

    update comments and formatting

--- main.c
+++ main.c
@@ -4,25 +4,25 @@
 
 int main()
 {
+  srand(time(NULL));		//initialize randomizer
   const int HIGH = 11; 		// Easy range edit for high
   const int LOW = 0;		// Easy range edit for low
-  int game = 1;
-  int correct = 0;
-  int cont = 1;
-  int guess = 0;
-  int answer = 0;
+  int correct = 0;			// if the guess is correct
+  int cont = 1;				// if user wants to continue
+  int guess = 0;			// user's guess
+  int answer = 0;			// the answer
 
 	printf("Guess a number between %d and %d\n", HIGH, LOW);
 	//Prompts user for input
-
-	srand(time(NULL));
-  answer = rand();
+	
+    answer = rand();
 	//Number generator
 
 	do {
 		printf("Your guess: ");
 		scanf("d%", guess);
 		printf("\n");
+		//get the user's answer
 
 		if (answer == guess)
 		{
@@ -36,6 +36,7 @@ int main()
 			scanf("%d",cont);
 			printf("\n");
 		}
+		//check the answer
 
 	} while (cont == 1);
 	//guessing
