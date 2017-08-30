ce3a4d016f74c3fdfa963e549623cd687653fb85
Ivan F <31392888+twistedfascination@users.noreply.github.com>
2017-08-29 20:32:07 -0700

    Update
    Update

--- main.c
+++ main.c
@@ -14,29 +14,29 @@ int main()
 
 	printf("Guess a number between %d and %d\n", HIGH, LOW);
 	//Prompts user for input
-	
+
 	answer = srand(time(NULL));
 	//Number generator
-	
+
 	do {
 		printf("Your guess: ");
 		scanf("d%", guess);
 		printf("\n");
-		
+
 		if (answer == guess)
 		{
 			correct = 1;
 			printf("You guessed right!\n");
 		}
-		else 
+		else
 		{
 			correct = 0;
 			printf("You guessed wrong. Try again? Y/N \n");
 			scanf("%d",cont);
 			printf("\n");
 		}
-		
+
 	} while (cont == 1);
 	//guessing
-	
+
 }
